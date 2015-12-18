d5e7962ddd39fb35bca023db92f10f55ef37a5c8
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-18 19:51:50 +0900

    var to let

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 let http = require('http');
-var server = http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain',
 		'charset': 'utf-8'
@@ -8,7 +8,7 @@ var server = http.createServer((req, res) => {
 	res.write(req.headers['user-agent']);
 	res.end();
 });
-var port = 8000;
+let port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
 });
