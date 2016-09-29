7c2b6a1126e767f1313cc803b401e6e13adf7012
DDRAGON <davide.0829@gmail.com>
2016-09-29 16:16:01 +0900

    インデントの修正

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain',
-		'charset': 'utf-8'
-	});
-	res.write(req.headers['user-agent']);
-	res.end();
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'charset': 'utf-8'
+  });
+  res.write(req.headers['user-agent']);
+  res.end();
 });
 const port = 8000;
 server.listen(port, () => {
